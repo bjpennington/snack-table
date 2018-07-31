213897c50abfcd4dfc5ca73d933fa69fae4ed239
BJ Pennington <bjpennington112@gmail.com>
2018-07-31 14:58:55 -0500

    add input snacks to redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      newSnack: ''
+    }
+  }
+
+  handleSnackChange = (event) => {
+    this.setState({
+      newSnack: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.handleSnackChange} type="text" />
+        <button onClick={() => this.props.dispatch({type: 'ADD_SNACK', payload: this.state.newSnack})}>Add your snack to the table!</button>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
